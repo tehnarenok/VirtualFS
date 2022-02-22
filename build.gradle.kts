1a441a007a6b10f783623dd33757079ed68c08bd
Mikhail Ershov <tehnarenok@gmail.com>
2022-02-22 18:31:58 +0300

    change build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,29 @@
 plugins {
-    application
+    `java-library`
 }
 
 group = "me.tehnarenok"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("junit:junit:4.13.1")
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
-    implementation("org.jetbrains:annotations:20.1.0")
-    implementation("org.jetbrains:annotations:20.1.0")
+    implementation("junit:junit:4.13.2")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation("org.jetbrains:annotations:22.0.0")
+}
+
+sourceSets {
+    main {
+        java.srcDir("src/main/java")
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.test {
